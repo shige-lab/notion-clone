03479fff342a0a5aba2e0694b2cbe36e04589cac
shige <shigebask@gmail.com>
2021-12-13 23:26:05 +0900

    make: reactの環境を簡易的に構築

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+        <div className='list'>
+          list
+        </div>
+        <div className='contents'>
+          contents
+        </div>
+    </div>
+    
+  );
+}
+export default Home;
